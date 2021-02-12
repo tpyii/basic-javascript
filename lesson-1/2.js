f15fdaf39ee82ed34b397078c56ed0c809ca76dc
Petr Komlev <petr.komlev@gmail.com>
2021-02-12 13:38:06 +0300

    lesson-1

---
+++ lesson-1/2.js
@@ -0,0 +1,6 @@
+'use strict';
+// Объявить две переменные: admin и name. Записать в name строку "Василий". Скопировать значение из name в admin. Вывести в консоль переменную admin (должно вывести "Василий").
+let admin, name;
+name = 'Василий';
+admin = name;
+console.log(admin);
